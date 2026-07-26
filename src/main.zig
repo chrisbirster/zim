59d0942ea8ab9ac56c092d9a47bf49d92535837c
Chris Birster <christopher.birster@gmail.com>
2026-07-26 14:42:23 -0400

    implemente command parser and help and version commands

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,53 @@ const Io = std.Io;
 
 const version = "0.1.0";
 
+const Command = enum {
+    version,
+    help,
+    workspace,
+
+    pub fn parse(s: []const u8) ?Command {
+        if (std.mem.eql(u8, s, "--version")) {
+            return .version;
+        }
+
+        if (std.mem.eql(u8, s, "-h") or std.mem.eql(u8, s, "--help")) {
+            return .help;
+        }
+
+        if (std.mem.eql(u8, s, ".")) {
+            return .workspace;
+        }
+
+        return null;
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
+    const allocator = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(allocator);
+
+    if (args.len < 2) {
+        try printHelp(init);
+        return;
+    }
+
+    const command = Command.parse(args[1]) orelse {
+        std.log.err("unknown command: {s}", .{args[1]});
+        try printHelp(init);
+        return;
+    };
+
+    switch (command) {
+        .help => try printHelp(init),
+        .version => try printHelp(
+            init,
+        ),
+        .workspace => try startWorkspace(init),
+    }
+}
+
+pub fn printHelp(init: std.process.Init) !void {
     const cwd = try std.process.currentPathAlloc(init.io, init.gpa);
     defer init.gpa.free(cwd);
 
@@ -29,4 +75,28 @@ pub fn main(init: std.process.Init) !void {
     , .{ version, cwd });
 
     try stdout.flush();
+}
+
+pub fn startWorkspace(init: std.process.Init) !void {
+    const cwd = try std.process.currentPathAlloc(init.io, init.gpa);
+    defer init.gpa.free(cwd);
+
+    try writeOutput(
+        init.io,
+        "Starting workspace in {s}\n",
+        .{cwd},
+    );
+}
+
+fn writeOutput(
+    io: std.Io,
+    comptime format: []const u8,
+    args: anytype,
+) !void {
+    var buffer: [4096]u8 = undefined;
+    var file_writer = std.Io.File.stdout().writer(io, &buffer);
+    const writer = &file_writer.interface;
+
+    try writer.print(format, args);
+    try writer.flush();
 }
