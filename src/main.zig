b11b95e9e130498a721c312779f1fadc63f72bd1
Chris Birster <christopher.birster@gmail.com>
2026-07-26 15:16:02 -0400

    Feat: Command level parsing (#1)
    * implemente command parser and help and version commands
    
    * update command parsing

--- src/main.zig
+++ src/main.zig
@@ -3,18 +3,63 @@ const Io = std.Io;
 
 const version = "0.1.0";
 
+const Command = enum {
+    version,
+    help,
+    workspace,
+
+    pub fn parse(s: []const u8) ?Command {
+        if (std.mem.eql(u8, s, "--version")) {
+            return .version;
+        }
+
+        if (std.mem.eql(u8, s, "-h") or std.mem.eql(u8, s, "--help")) {
+            return .help;
+        }
+
+        if (std.mem.eql(u8, s, ".")) {
+            return .workspace;
+        }
+
+        return null;
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
+    const allocator = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(allocator);
+
+    if (args.len < 2) {
+        try printHelp(init);
+        return;
+    }
+
+    const command = Command.parse(args[1]) orelse {
+        std.log.err("unknown command: {s}", .{args[1]});
+        try printHelp(init);
+        return;
+    };
+
+    switch (command) {
+        .help => try printHelp(init),
+        .version => try printVersion(init),
+        .workspace => try startWorkspace(init),
+    }
+}
+
+pub fn printVersion(init: std.process.Init) !void {
+    try writeOutput(
+        init.io,
+        "ZIM version {s}\n",
+        .{version},
+    );
+}
+
+pub fn printHelp(init: std.process.Init) !void {
     const cwd = try std.process.currentPathAlloc(init.io, init.gpa);
     defer init.gpa.free(cwd);
 
-    var stdout_buffer: [4096]u8 = undefined;
-    var stdout_writer = std.Io.File.stdout().writer(
-        init.io,
-        &stdout_buffer,
-    );
-
-    const stdout = &stdout_writer.interface;
-    try stdout.print(
+    const help_template =
         \\
         \\ZIM - {s} - YOUR NEW CODE OVERLORD 
         \\
@@ -26,7 +71,35 @@ pub fn main(init: std.process.Init) !void {
         \\[STARTING]    Operation Impending Build 
         \\[READY]       Commence coding
         \\
-    , .{ version, cwd });
+    ;
 
-    try stdout.flush();
+    try writeOutput(
+        init.io,
+        help_template,
+        .{ version, cwd },
+    );
+}
+
+pub fn startWorkspace(init: std.process.Init) !void {
+    const cwd = try std.process.currentPathAlloc(init.io, init.gpa);
+    defer init.gpa.free(cwd);
+
+    try writeOutput(
+        init.io,
+        "Starting workspace in {s}\n",
+        .{cwd},
+    );
+}
+
+fn writeOutput(
+    io: std.Io,
+    comptime format: []const u8,
+    args: anytype,
+) !void {
+    var buffer: [4096]u8 = undefined;
+    var file_writer = std.Io.File.stdout().writer(io, &buffer);
+    const writer = &file_writer.interface;
+
+    try writer.print(format, args);
+    try writer.flush();
 }
