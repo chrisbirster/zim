80c7af166452d79a783615c27789028ca581a815
Chris Birster <christopher.birster@gmail.com>
2026-09-02 14:06:32 -0400

    test(api): exercise stable API entrypoint

--- src/core_tests.zig
+++ src/core_tests.zig
@@ -3,7 +3,7 @@ test {
     _ = @import("workspace.zig");
     _ = @import("cli.zig");
     _ = @import("editor.zig");
-    _ = @import("api/root.zig");
+    _ = @import("api.zig");
     _ = @import("language_bridge.zig");
     _ = @import("lsp/root.zig");
     _ = @import("lsp_bridge.zig");
